73f70bd35d899e87f4a7556128685329999a5343
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-07-24 17:59:55 -0300

    Eslint, Prettier and editorconfig configuration

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
