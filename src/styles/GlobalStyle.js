863fc74d6e1704aead46ffe4e078af37da5b57ac
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-08-08 09:46:57 -0300

    Add global styles

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import { primaryColor, primaryDarkColor } from '../config/colors';
 
 export default createGlobalStyle`
   * {
@@ -8,9 +9,10 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body: {
+  body {
     font-family: sans-serif;
-    background: #eee;
+    background: ${primaryDarkColor};
+    color: ${primaryDarkColor};
   }
 
   html, body, #root {
@@ -19,10 +21,17 @@ export default createGlobalStyle`
 
   button {
     cursor: pointer;
+    background: ${primaryColor};
+    border: none;
+    color: #fff;
+    padding: 10px 20px;
+    border-radius: 4px;
+    font-weight: 700;
   }
 
   a {
     text-decoration: none;
+    background: ${primaryColor};
   }
 
   ul {
