4ca624c7036e17093ddc3207f0b17b0e120c65b3
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-08-14 15:30:39 -0300

    Config axios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 function Routes() {
-  toast.success('Sucesso');
-  toast.error('Fracasso');
-
   return (
     <Switch>
       <MyRoute exact path='/' component={Login} />
